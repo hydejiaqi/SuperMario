c28544f05c58d75148385ce84dc9e478c7856a80
hydejiaqi <hydejiaqi@gmail.com>
2017-07-28 09:33:21 -0700

    Initial commit

---
+++ android/src/com/mygdx/supermario/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mygdx.supermario.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.supermario.SuperMarioDemo;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SuperMarioDemo(), config);
+	}
+}
---
+++ desktop/src/com/mygdx/supermario/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.supermario.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.supermario.SuperMarioDemo;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SuperMarioDemo(), config);
+	}
+}
