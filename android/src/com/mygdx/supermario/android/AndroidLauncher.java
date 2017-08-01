b8374da9ae224303a4f4dc5e704ed8fb62d52d1d
jjiang@autelrobotics.com <jjiang@autelrobotics>
2017-08-01 10:35:06 -0700

    before tile map

--- android/src/com/mygdx/supermario/android/AndroidLauncher.java
+++ android/src/com/mygdx/supermario/android/AndroidLauncher.java
@@ -4,13 +4,13 @@ import android.os.Bundle;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-import com.mygdx.supermario.SuperMarioDemo;
+import com.mygdx.supermario.SuperMario;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		initialize(new SuperMarioDemo(), config);
+		initialize(new SuperMario(), config);
 	}
 }
--- desktop/src/com/mygdx/supermario/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/supermario/desktop/DesktopLauncher.java
@@ -2,11 +2,11 @@ package com.mygdx.supermario.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.supermario.SuperMarioDemo;
+import com.mygdx.supermario.SuperMario;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SuperMarioDemo(), config);
+		new LwjglApplication(new SuperMario(), config);
 	}
 }
